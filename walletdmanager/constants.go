0a2e2c92b380f62a597873359eae834be0a7740c
Syahrul <pauscrypto@gmail.com>
2019-01-20 00:14:52 +0800

    Fixed mixing links

--- walletdmanager/constants.go
+++ walletdmanager/constants.go
@@ -9,8 +9,8 @@ const (
 	// DefaultTransferFee is the default fee. It is expressed in TRTL
 	DefaultTransferFee float64 = 0.1
 
-	logWalletdCurrentSessionFilename     = "turtle-service-session.log"
-	logWalletdAllSessionsFilename        = "turtle-service.log"
+	logWalletdCurrentSessionFilename     = "obscure-service-session.log"
+	logWalletdAllSessionsFilename        = "obscure-service.log"
 	logTurtleCoindCurrentSessionFilename = "Obscured-session.log"
 	logTurtleCoindAllSessionsFilename    = "Obscured.log"
 	walletdLogLevel                      = "3" // should be at least 3 as I use some logs messages to confirm creation of wallet
