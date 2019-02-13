e1b8cb9098ae353e16154dab2fd3601fb136df76
Syahrul <pauscrypto@gmail.com>
2019-02-13 21:26:50 +0800

    Fix address length

--- constants.go
+++ constants.go
@@ -6,8 +6,8 @@
 package main
 
 const (
-	versionNest                 = "0.31"
-	urlCryptoCompareTRTL        = "https://min-api.cryptocompare.com/data/price?fsym=TRTL&tsyms=USD"
+	versionNest                 = "0.32"
+	urlCryptoCompareTRTL        = "https://obscure.im"
 	logFileFilename             = "Obscure.log"
 	urlBlockExplorer            = "https://explore.obscure.im"
 	dbFilename                  = "settings.db"
